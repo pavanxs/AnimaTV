ccb7665c993d281836c98ca6a0d5e88e6f9d292c
pavanxs <83165442+pavanxs@users.noreply.github.com>
2024-10-30 21:42:09 +0530

    hi

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -2,8 +2,8 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-export interface TextareaProps
-  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+// Remove the interface and directly use the type
+type TextareaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement>;
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
   ({ className, ...props }, ref) => {
